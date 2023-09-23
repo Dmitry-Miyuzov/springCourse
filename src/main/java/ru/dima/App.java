e44bfc4ccdfac193b145df39c82b8546ecdbf205
dmiyuzov <dmitry.miyuzov@gmail.com>
2023-09-24 00:41:52 +0300

    база для лабораторных

---
+++ src/main/java/ru/dima/App.java
@@ -0,0 +1,11 @@
+package ru.dima;
+
+public class App {
+
+    public static void main(String[] args) {
+//        ApplicationContext contextJavaConfig = new AnnotationConfigApplicationContext("ru.dima");
+//        ApplicationContext contextXmlConfig = new ClassPathXmlApplicationContext("applicationContext.xml");
+    }
+
+
+}
--- src/main/java/ru/dima/Test.java
+++
@@ -1,4 +0,0 @@
-package ru.dima;
-
-public class Test {
-}
